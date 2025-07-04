75c9e95c4d94edf6861ad39015d01c395dbeed29
Seongho <chzhqk98@naver.com>
2025-07-04 09:54:31 +0900

    feat : print hello world

---
+++ helloworld.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int hello_world(){
+    char word = "Hello, World";
+    printf(word);
+    getchar();
+}
--- learning.c
+++ learning.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    int array[] = {1, 2, 3, 4};
-
-    printf("배열의 시작 주소: %p\n", (void*)array);
-    printf("첫 번째 값: %d\n", array[0]); 
-
-
+    char* word ="Hello,World";
+    printf("%s",word);
     getchar();
     return 0;
 }
